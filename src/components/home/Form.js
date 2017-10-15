5ea1563fb0ad97af2d6ccb94209a4e46d345c456
Ilan Ben <ibenichou@eleven-labs.com>
2017-10-15 10:16:09 +0200

    Implement Button

--- src/components/home/Form.js
+++ src/components/home/Form.js
@@ -8,8 +8,13 @@ import {
 } from 'react-native';
 
 import Input from '../form/Input';
+import Btn from '../form/Btn';
 
 export default class Form extends Component<{}> {
+    search() {
+        console.log("OK OK")
+    }
+
     render() {
         let color = (Platform.OS === 'ios') ? "#FFF" : "#FF2E2E"
 
