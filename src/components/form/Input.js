35f095c3c5879885cc153c852557fabde6071353
Ilan Ben <ibenichou@eleven-labs.com>
2017-10-15 09:29:31 +0200

    Finish first exo

---
+++ src/components/form/Input.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+import {
+  StyleSheet,
+  TextInput,
+} from 'react-native';
+
+export default class Input extends Component<{}> {
+    constructor(props) {
+        super(props);
+        this.state = { placeholder: this.props.placeholder };
+    }
+
+    render() {
+        return (
+            <TextInput
+                style={styles.input}
+                onChangeText={(text) => this.setState({text})}
+                value={this.state.text}
+                placeholder={this.state.placeholder}
+                underlineColorAndroid='rgba(0,0,0,0)'
+            />
+        );
+    }
+}
+
+const styles = StyleSheet.create({
+    input: {
+        marginTop: 0,
+        borderBottomColor: '#DDDDDD',
+        borderBottomWidth: 1,
+        height: 47,
+        paddingLeft: 11.5,
+    }
+});
