86e99e862e1774ebf09b567da6d90687664550e6
Ilan Ben <ibenichou@eleven-labs.com>
2017-10-15 09:29:31 +0200

    Finish first exo

--- src/components/home/Form.js
+++ src/components/home/Form.js
@@ -53,7 +53,7 @@ export default class Form extends Component<{}> {
           })
           .catch(function (error) {
             console.log("ERROR:", error);
-          });
+        });
     }
 
     changeWhere(whereValue) {
